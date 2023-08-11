bec952b07e0c8f4db240d9b695b9dd02ce0aef0d
RSL <RSL@LAPTOP-VQ4R2BLH>
2023-08-11 13:44:25 +0530

    Add project files.

---
+++ SingletonDemo/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    internal class Class1
+    {
+        private static Class1 instance = null;
+        private static readonly object padlock= new object(); 
+
+        private Class1()
+        {
+            Console.WriteLine("Constructor Called");
+        }
+
+        public static Class1 Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    Console.WriteLine("Instance Called");
+                    if (instance == null)
+                    {
+                        instance = new Class1();
+                    }
+                    return instance;
+                }
+
+            }
+        }
+    }
+}
---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using SingletonDemo;
+
+Parallel.Invoke(
+    () => CreateInstance(),
+    () => CreateInstance(),
+    () => CreateInstance()
+    //() => CreateInstance2()  
+    );
+
+static void CreateInstance()
+{
+    
+    var instance = Class1.Instance;
+    Console.ReadLine();
+}
+
+/*static void CreateInstance2()
+{
+    Thread.Sleep(500);
+    var instance = Class1.Instance;
+    Console.ReadLine();
+}*/
+
